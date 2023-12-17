f9eb41bd84d3965b23f90b7e8c7d9fff72a70fd7
Gheorghe Dandiș <dvndis.gheorghe@gmail.com>
2023-12-17 12:49:39 +0200

    Changed category controller to get data from service instead repository

--- Laboratorul 5/TODO-RestAPI-Server v17/src/main/java/TODO/RestAPI/Server/controller/CategoryController.java
+++ Laboratorul 5/TODO-RestAPI-Server v17/src/main/java/TODO/RestAPI/Server/controller/CategoryController.java
@@ -1,9 +1,9 @@
 package TODO.RestAPI.Server.controller;
 
 import TODO.RestAPI.Server.entity.Category;
+import TODO.RestAPI.Server.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import TODO.RestAPI.Server.repository.CategoryRepository;
 
 import java.util.List;
 
@@ -12,32 +12,31 @@ import java.util.List;
 public class CategoryController {
 
     @Autowired
-    private CategoryRepository categoryRepository;
+    private CategoryService categoryService;
 
     @GetMapping
     public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
+        return categoryService.getAllCategories();
     }
 
     @GetMapping("/{categoryId}")
     public Category getCategoryById(@PathVariable Long categoryId) {
-        return categoryRepository.findById(categoryId).orElse(null);
+        return categoryService.getCategoryById(categoryId).orElse(null);
     }
 
     @PostMapping
     public Category createCategory(@RequestBody Category category) {
-        return categoryRepository.save(category);
+        return categoryService.createCategory(category);
     }
 
     @PutMapping("/{categoryId}")
     public Category updateCategory(@PathVariable Long categoryId, @RequestBody Category updatedCategory) {
-        updatedCategory.setId(categoryId);
-        return categoryRepository.save(updatedCategory);
+        return categoryService.updateCategory(categoryId,updatedCategory);
     }
 
     @DeleteMapping("/{categoryId}")
     public void deleteCategory(@PathVariable Long categoryId) {
-        categoryRepository.deleteById(categoryId);
+        categoryService.deleteCategory(categoryId);
     }
 }
 
--- Laboratorul 5/TODO-RestAPI-Server v17/src/main/java/TODO/RestAPI/Server/entity/User.java
+++ Laboratorul 5/TODO-RestAPI-Server v17/src/main/java/TODO/RestAPI/Server/entity/User.java
@@ -2,73 +2,15 @@ package TODO.RestAPI.Server.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+
 @Data
 @Entity
-@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "username")
     private String username;
-    @Column(name = "email")
     private String email;
-
-    public User(Long id, String username, String email) {
-        this.id = id;
-        this.username = username;
-        this.email = email;
-    }
-
-    public User() {
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public boolean equals(final Object o) {
-        if (o == this) return true;
-        if (!(o instanceof User)) return false;
-        final User other = (User) o;
-        if (!other.canEqual((Object) this)) return false;
-        final Object this$id = this.getId();
-        final Object other$id = other.getId();
-        if (this$id == null ? other$id != null : !this$id.equals(other$id)) return false;
-        final Object this$username = this.getUsername();
-        final Object other$username = other.getUsername();
-        if (this$username == null ? other$username != null : !this$username.equals(other$username)) return false;
-        final Object this$email = this.getEmail();
-        final Object other$email = other.getEmail();
-        if (this$email == null ? other$email != null : !this$email.equals(other$email)) return false;
-        return true;
-    }
-
-    protected boolean canEqual(final Object other) {
-        return other instanceof User;
-    }
-
-    public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        final Object $id = this.getId();
-        result = result * PRIME + ($id == null ? 43 : $id.hashCode());
-        final Object $username = this.getUsername();
-        result = result * PRIME + ($username == null ? 43 : $username.hashCode());
-        final Object $email = this.getEmail();
-        result = result * PRIME + ($email == null ? 43 : $email.hashCode());
-        return result;
-    }
-
-    public String toString() {
-        return "User(id=" + this.getId() + ", username=" + this.getUsername() + ", email=" + this.getEmail() + ")";
-    }
 }
