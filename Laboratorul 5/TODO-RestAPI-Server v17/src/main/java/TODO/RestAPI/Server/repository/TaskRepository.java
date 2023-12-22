9de77a8253ccaf32430f077bbbfdde52f94ca93f
Gheorghe Dandiș <dvndis.gheorghe@gmail.com>
2023-12-22 15:07:55 +0200

    Removed unused repositories

--- Laboratorul 5/TODO-RestAPI-Server v17/src/main/java/TODO/RestAPI/Server/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package TODO.RestAPI.Server.repository;
-
-import TODO.RestAPI.Server.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
